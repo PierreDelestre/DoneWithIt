33bf8bc94bd98f0cbdee2d5d88e1ba359e01c70e
DELESTRE Pierre EXT <pierre.delestre-prestataire@laposte.fr>
2025-07-27 16:43:37 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import { View } from 'react-native';
+import WelcomeScreen from './app/screens/WelcomeScreen';
+
+export default function App() {
+  console.log("App executed!");
+
+  return (
+    <WelcomeScreen />
+  );
+}
+
