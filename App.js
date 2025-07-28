e0224adc08b66ea1624058cd1f8596db88fd9492
DELESTRE Pierre EXT <pierre.delestre-prestataire@laposte.fr>
2025-07-28 14:26:06 +0100

    Adds ViewImageScreen.js

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { View } from 'react-native';
 import WelcomeScreen from './app/screens/WelcomeScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 
 export default function App() {
   console.log("App executed!");
 
   return (
-    <WelcomeScreen />
+    //<WelcomeScreen />
+    <ViewImageScreen />
   );
 }
 
