227366ee4d897d75da1929426bc61c4cf4a49389
DELESTRE Pierre EXT <pierre.delestre-prestataire@laposte.fr>
2025-07-30 17:36:42 +0100

    Adds ListingDetailsScreen

--- App.js
+++ App.js
@@ -2,24 +2,13 @@ import { View } from 'react-native';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import AppButton from './app/components/AppButton';
 import Card from './app/components/Card';
+import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 
 export default function App() {
   console.log("App executed!");
 
   return (
-    <View style={{
-      backgroundColor: '#f8f4f4',
-      padding: 20,
-      paddingTop: 100,
-
-    }}>
-      <Card
-        title="Red jacket for sale" 
-        subTitle="£79" 
-        image={require('./app/assets/jacket.jpg')}
-      />
-
-    </View>
+    <ListingDetailsScreen />
   );
 }
 
--- app/components/AppText/styles.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet, Platform } from 'react-native'
-
-const styles = StyleSheet.create({
-    text: {
-        fontSize: 18,
-        fontFamily: Platform.OS === 'android' ? 'roboto' : 'avenir',
-
-    }
-});
