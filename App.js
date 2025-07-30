f63998b176b5b2f304de22835c9f7962418650b2
DELESTRE Pierre EXT <pierre.delestre-prestataire@laposte.fr>
2025-07-30 18:17:10 +0100

    Adds MaterialCommunityIcons to ViewImageScreen

--- App.js
+++ App.js
@@ -4,11 +4,13 @@ import AppButton from './app/components/AppButton';
 import Card from './app/components/Card';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 
+import ViewImageScreen from './app/screens/ViewImageScreen';
+
 export default function App() {
   console.log("App executed!");
 
   return (
-    <ListingDetailsScreen />
+    <ViewImageScreen />
   );
 }
 
