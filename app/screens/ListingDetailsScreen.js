99b525b34a57f7af2325d3c02d522d7ed0016cc2
DELESTRE Pierre EXT <pierre.delestre-prestataire@laposte.fr>
2025-07-30 18:09:19 +0100

    Adds ListItem component

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Image, StyleSheet } from 'react-native';
 import AppText from '../components/AppText';
+import ListItem from '../components/ListItem';
 
 import colors from '../config/colors';
 
@@ -11,8 +12,14 @@ function ListingDetailsScreen(props) {
             <View style={styles.detailsContainer}>
                 <AppText style={styles.title}>Read jacket for sale</AppText>
                 <AppText style={styles.price}>£200</AppText>
+                <View style={styles.userContainer}>
+                    <ListItem
+                        image={require('../assets/pierre.jpg')}
+                        title="Pierre"
+                        subTitle="5 Listings"
+                    />
+                </View>
             </View>
-            
         </View>
     );
 }
@@ -34,6 +41,9 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 24,
         fontWeight: '500'
+    },
+    userContainer: {
+        marginVertical: 40
     }
 })
 
