c12bbb32f0bfa0327890de9d93c5c26a2fdbf4ed
DELESTRE Pierre EXT <pierre.delestre-prestataire@laposte.fr>
2025-07-30 17:06:09 +0100

    Adds Card component

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import { View } from 'react-native';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import AppButton from './app/components/AppButton';
+import Card from './app/components/Card';
 
 export default function App() {
   console.log("App executed!");
 
   return (
-    <WelcomeScreen />
+    <View style={{
+      backgroundColor: '#f8f4f4',
+      padding: 20,
+      paddingTop: 100,
+
+    }}>
+      <Card
+        title="Red jacket for sale" 
+        subTitle="£79" 
+        image={require('./app/assets/jacket.jpg')}
+      />
+
+    </View>
   );
 }
 
