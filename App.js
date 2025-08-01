d6a428d319c1da41cd464f249a9a5162801421a8
DELESTRE Pierre EXT <pierre.delestre-prestataire@laposte.fr>
2025-08-01 13:45:52 +0100

    Adds MessagesScreen screen and Screen component

--- App.js
+++ App.js
@@ -1,16 +1,10 @@
-import { View } from 'react-native';
-import WelcomeScreen from './app/screens/WelcomeScreen';
-import AppButton from './app/components/AppButton';
-import Card from './app/components/Card';
-import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
-
-import ViewImageScreen from './app/screens/ViewImageScreen';
+import MessagesScreen from './app/screens/MessagesScreen';
 
 export default function App() {
   console.log("App executed!");
 
   return (
-    <ViewImageScreen />
+    <MessagesScreen />
   );
 }
 
